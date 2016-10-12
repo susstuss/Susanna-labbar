4fd4167cde9e6e5470a40a1a3bc1f4ac06727bf0
susanna elofsson <susanna elofsson>
2016-10-12 11:51:17 +0200

    initial commit

---
+++ DogList/DogList/Runtime.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogList
+{
+    class Runtime
+    {
+        public void TheDogs()
+        {
+            Dog dog1 = new Dog
+            {
+                Name = "Snobben",
+                Age = 5,
+                Breed = "Beagle"
+            };
+
+            Dog dog2 = new Dog
+            {
+                Name = "Lassie",
+                Age = 3,
+                Breed = "Collie"
+            };
+
+            Dog dog3 = new 
+
+        }
+    }
+}
